c268af0627dcc1f231a0922f72a2a83069a11206
kyeonjuk <yeonjuminju@gmail.com>
2025-04-14 17:40:02 +0900

    fix : 4-3) Kakao Social Login 3 - 오류 수정
    - 예외 처리 문구 변경

--- src/main/java/com/example/auth_sample/service/LoginService.java
+++ src/main/java/com/example/auth_sample/service/LoginService.java
@@ -13,17 +13,18 @@ public class LoginService {
     private final KakaoService kakaoService;
     private final EmployeeService employeeService;
 
-    public ResponseEntity login(String code) {
+    public ResponseEntity<String> login(String code) {
         String token = kakaoService.getAccessTokenFromKakao(code);
         KakaoUserInfoResponseDto dto = kakaoService.getUserFromKakao(token);
 
         String nickName = dto.getKakaoAccount().getProfile().getNickName();
+        System.out.println(nickName);
 
         // 인사팀에서 등록시킨 회원만 카카오 로그인 가능
         if (employeeService.existsByKakaoNickName(nickName)) {
-            return new ResponseEntity("환영합니다. " + nickName + "님" + dto, HttpStatus.OK);
+            return new ResponseEntity<>("환영합니다. " + nickName + "님", HttpStatus.OK);
         } else {
-            return new ResponseEntity("", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("등록된 임직원이 아닙니다.", HttpStatus.FORBIDDEN);
         }
     }
 }
